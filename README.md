85b3a13db299e54fa0ed699bbc6bd63968801065
unknown <xiaolong.feng@yitu-inc.com>
2019-07-11 19:54:29 +0800

    创建我的项目

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Notes
+
+记录我整理的一些学习资料
---
+++ src/pages/dashboard/workplace/index.tsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+class Workplace extends PureComponent{
+  render(){
+    return (<div>测试数据</div>)
+  }
+}
+
+export default Workplace;
