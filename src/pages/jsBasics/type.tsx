f7c15a3107fd0ccf839c158168bd467158c7a3d7
unknown <xiaolong.feng@yitu-inc.com>
2019-07-18 15:57:12 +0800

    add: javaScript变量和类型

--- src/pages/jsBasics/type.tsx
+++ src/pages/jsBasics/type.tsx
@@ -1,8 +1,30 @@
 import React, { PureComponent } from 'react';
+import { Card, Row, Col } from 'antd'
+import styles from './index.scss'
 
-class JsType extends PureComponent{
-  render(){
-    return (<div>测试数据</div>)
+class JsType extends PureComponent {
+  render() {
+    return (<>
+
+      <Row>
+        <Col span={19} className={styles.padding_12}>
+          <Card
+            title="javaScript变量和类型"
+            extra={<a href="#">More</a>}
+            bordered={false}>
+            这边是文章的主要内容
+            </Card>
+        </Col>
+        <Col span={5}>
+          <Card
+            title="javaScript变量和类型"
+            extra={<a href="#">More</a>}
+            bordered={false}>
+            这边是文章的大概列表
+            </Card>
+        </Col>
+      </Row>
+    </>)
   }
 }
 
