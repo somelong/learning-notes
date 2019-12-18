8bcf5189640d0144bf2a3051aaed508d7a037d7c
somelong <330059665@qq.com>
2019-12-18 23:23:12 +0800

    删除了git commit 校验文件 pre-commit

--- src/pages/dashboard/analysis/index.tsx
+++ src/pages/dashboard/analysis/index.tsx
@@ -1,9 +1,9 @@
 import React, { PureComponent } from 'react';
 
-class Workplace extends PureComponent{
-  render(){
-    return (<div>这个是第二个页面</div>)
+class analysis extends PureComponent {
+  render() {
+    return <div>这是第一个页面</div>;
   }
 }
 
-export default Workplace;
+export default analysis;
--- src/pages/dashboard/workplace/index.tsx
+++ src/pages/dashboard/workplace/index.tsx
@@ -1,8 +1,8 @@
 import React, { PureComponent } from 'react';
 
-class Workplace extends PureComponent{
-  render(){
-    return (<div>测试数据</div>)
+class Workplace extends PureComponent {
+  render() {
+    return <div>这个是第二个页面</div>;
   }
 }
 
